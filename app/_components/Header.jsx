736e22e01d136d747c6cfe76ad8477e95a0beaad
fahimnirjon <fahimnirjon21@gmail.com>
2024-11-16 20:56:55 +0600

    added

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
   return (
     <div className="p-5 flex justify-between items-center border shadow-sm">
       <div className="flex flex-row items-center">
-        <Link to='/dashboard' >
+        <Link href={"/dashboard"} >
         <Image src={"/chart-donut.svg"} alt="logo" width={40} height={25} />
         <span className="text-blue-800  font-bold text-xl">FinanceSmartly</span></Link>
       </div>
